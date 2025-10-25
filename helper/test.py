c5e203460a602ce40138fec591568df63672fc8d
Hasnain <hasnain365syed@gmail.com>
2025-10-25 18:04:36 -0400

    gradio restart

--- helper/test.py
+++ helper/test.py
@@ -1,16 +1,41 @@
-from core.agent_manager import AgentManager
+import gradio as gr
+import random
+import time
 
-manager = AgentManager()
-initial_prompt = "Let's debate the risks and rewards of large-scale solar power deployment."
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    @gr.on([greet_btn.click, name.submit], inputs=name, outputs=output)
+    def greet(name):
+        return "Hello " + name + "!"
+    
+    @gr.render(inputs=name, triggers=[output.change])
+    def spell_out(name):
+        with gr.Row():
+            for letter in name:
+                gr.Textbox(letter)
 
-# Run the first turn (Agent A starts)
-response_a = manager.run_turn(initial_prompt)
-print(response_a)
+with demo.route("Up") as incrementer_demo:
+    num = gr.Number()
+    incrementer_demo.load(lambda: time.sleep(1) or random.randint(10, 40), None, num)
 
-# Run the second turn (Agent B responds)
-response_b = manager.run_turn(response_a)
-print(response_b)
+    with gr.Row():
+        inc_btn = gr.Button("Increase")
+        dec_btn = gr.Button("Decrease")
+    inc_btn.click(fn=lambda x: x + 1, inputs=num, outputs=num, api_name="increment")
+    dec_btn.click(fn=lambda x: x - 1, inputs=num, outputs=num, api_name="decrement")
+    for i in range(100):
+        gr.Textbox()
 
-print("this is a test")
+def wait(x):
+    time.sleep(2)
+    return x
 
-print("ricky learning how to rebase")
+identity_iface = gr.Interface(wait, "image", "image")
+
+with demo.route("Interface") as incrementer_demo:
+    identity_iface.render()
+    gr.Interface(lambda x, y: x * y, ["number", "number"], "number")
+
+demo.launch()
