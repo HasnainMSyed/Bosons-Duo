42bd65789877f7b1ca68fc39471916daabcce451
Eric Huang <xilahuade@foxmail.com>
2025-10-25 16:42:16 -0400

    audio generation

--- helper.py
+++ helper.py
@@ -6,9 +6,11 @@ audio = AudioSegment.from_wav("audio_references/davis_reference_full.wav")
 # Get duration in milliseconds
 duration_ms = len(audio)
 
-cut = audio[3 * duration_ms // 4 :duration_ms * 4/5.1]
+# cut = audio[2.78 * duration_ms // 4 :duration_ms * 4/5.1]
+cut = audio[52000:92000]
+
 
 # Export to new file
-cut.export("audio_references/davis_trimmed_quarter.wav", format="wav")
+cut.export("audio_references/davis_trimmed.wav", format="wav")
 
 print("Saved trimmed audio as trimmed.wav")
